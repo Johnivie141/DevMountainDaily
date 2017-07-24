3a400c815dc64cb527d1975901c32250a94567ff
john <john.ivie@gmail.com>
2017-07-24 09:26:09 -0600

    Testing WebStorm Commit

--- Jul24/Disemvoweler.js
+++ Jul24/Disemvoweler.js
@@ -9,6 +9,19 @@ return str.replace(/[aeiouAEIOU]/g,"");
 
 }
 
+/* Tutor
+function remVowel(str){
+var out=[];
+var vowels=[aeiuoAEIOU];
+for (var i=0;i< str.length;i++)
+{
+   if (vowels.indexOf(str[i])==-1){
+    out.push(str[i]);
+   }
+
+
+ */
+
 //expected "Ths wbst s fr lsrs LL!".
 var ans=Disemvoweler("This website is for losers LOL!");
 console.log(ans);
